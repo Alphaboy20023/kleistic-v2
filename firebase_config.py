e294c33d319ad87f5fc942aff9b7f29d21cb53e5
Alphaboy20023 <timivictor565@gmail.com>
2025-07-07 23:00:20 +0200

    fuck man

--- firebase_config.py
+++ firebase_config.py
@@ -4,13 +4,13 @@ import os
 import json
 
 def init_firebase():
-    if not firebase_admin._apps:  
+    if not firebase_admin._apps:
         firebase_creds_str = os.environ.get("FIREBASE_CREDS_JSON")
-
         if not firebase_creds_str:
-            raise ValueError("Missing FIREBASE_CREDS_JSON in environment")
+            raise ValueError("Missing FIREBASE_CREDS_JSON environment variable")
 
-       
+        # Convert string to dictionary
         cred_dict = json.loads(firebase_creds_str)
-        cred = credentials.Certificate(cred_dict)
+        cred = credentials.Certificate(cred_dict)  
+
         firebase_admin.initialize_app(cred)
