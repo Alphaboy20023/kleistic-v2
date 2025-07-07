9981242bd9c7392c9d9e82fcd898709f11a729a3
Alphaboy20023 <timivictor565@gmail.com>
2025-07-07 21:54:19 +0200

    firebase config

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
+import json
 
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")
+        firebase_creds = os.getenv('FIREBASE_CREDS_JSON')
+        if not firebase_creds:
+            raise Exception("FIREBASE_CREDS_JSON is missing")
+        cred = credentials.Certificate(json.loads(firebase_creds))
         firebase_admin.initialize_app(cred)
